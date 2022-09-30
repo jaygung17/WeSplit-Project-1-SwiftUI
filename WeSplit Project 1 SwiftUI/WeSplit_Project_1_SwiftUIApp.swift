0cdd81a7915f878d6f1642f4eb9ac084a8322bc4
Novan Agung Waskito <agung.luo@gmail.com>
2022-09-30 16:16:09 +0700

    Initial Commit

---
+++ WeSplit Project 1 SwiftUI/WeSplit_Project_1_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_Project_1_SwiftUIApp.swift
+//  WeSplit Project 1 SwiftUI
+//
+//  Created by Novan Agung Waskito on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_Project_1_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
