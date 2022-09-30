3a6c0f73bf9f464d1f67c578b338fda858730e2f
Novan Agung Waskito <agung.luo@gmail.com>
2022-09-30 17:58:05 +0700

    Form, Section, Text Field, Group, Text

--- WeSplit Project 1 SwiftUI/ContentView.swift
+++ WeSplit Project 1 SwiftUI/ContentView.swift
@@ -8,16 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter your name", text: $name)
+                    Text("Your Icloud is \(name)")
+                }
+                Section {
+                    Text("Airplane Mode")
+                    Text("Wi-Fi")
+                    Text("Bluetooth")
+                    Text("Cellular")
+                    Text("Personal Hotspot")
+                    Text("VPN")
+                }
+                .navigationTitle("Settings")
+                Section {
+                    Text("Notifications")
+                    Text("Sounds & Haptics")
+                    Text("Focus")
+                    Text("Screen Time")
+                }
+                    Section {
+                        Group {
+                            Text("General")
+                            Text("Control Center")
+                            Text("Display & Brightness")
+                            Text("Home Screen")
+                            Text("Accessibility")
+                            Text("Wallpaper")
+                        }
+                        Group {
+                            Text("Siri & Search")
+                            Text("Face ID & Passcode")
+                            Text("Emergency SOS")
+                            Text("Exposure Notifications")
+                            Text("Battery")
+                            Text("Privacy & Security")
+                        }
+                    }
+                }
+                }
+            }
         }
-        .padding()
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
